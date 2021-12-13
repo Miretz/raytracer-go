e711f8aa24748dca72a934e0bedc9012af1a6c57
miretz <1217795+Miretz@users.noreply.github.com>
2021-12-13 16:36:43 +0100

    Final Render - Unoptimized

--- main.go
+++ main.go
@@ -5,55 +5,88 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 )
 
-func writePPM() {
+func randomScene() hittable_list {
+	world := hittable_list{}
+
+	var groundMateial material = &lambertian{color{0.5, 0.5, 0.5}}
+	world.Add(&sphere{point3{0, -1000, 0}, 1000, &groundMateial})
+
+	for a := -11; a < 11; a++ {
+		for b := -11; b < 11; b++ {
+			chooseMat := RandomFloat()
+			center := point3{float64(a) + 0.9*RandomFloat(),
+				0.2, float64(b) + 0.9*RandomFloat()}
+
+			subPoint := center.Sub(&point3{4, 0.2, 0})
+			if subPoint.Length() > 0.9 {
+				var sphereMaterial material = nil
+
+				if chooseMat < 0.8 {
+					// diffuse
+					r1 := Vec3_Random()
+					r2 := Vec3_Random()
+					albedo := Vec3_Mul(&r1, &r2)
+					sphereMaterial = &lambertian{albedo}
+					world.Add(&sphere{center, 0.2, &sphereMaterial})
+				} else if chooseMat < 0.95 {
+					// metal
+					albedo := Vec3_RandomBetween(0.5, 1)
+					fuzz := RandomFloatBetween(0, 0.5)
+					sphereMaterial = &metal{albedo, fuzz}
+					world.Add(&sphere{center, 0.2, &sphereMaterial})
+				} else {
+					// glass
+					sphereMaterial = &dielectric{1.5}
+					world.Add(&sphere{center, 0.2, &sphereMaterial})
+				}
+
+			}
+		}
+	}
+
+	var material1 material = &dielectric{1.5}
+	world.Add(&sphere{point3{0, 1, 0}, 1.0, &material1})
+
+	var material2 material = &lambertian{color{0.4, 0.2, 0.1}}
+	world.Add(&sphere{point3{-4, 1, 0}, 1.0, &material2})
+
+	var material3 material = &metal{color{0.7, 0.6, 0.5}, 0.0}
+	world.Add(&sphere{point3{4, 1, 0}, 1.0, &material3})
+
+	return world
+}
+
+func render() {
 
 	// Image
-	const aspectRatio = 16.0 / 9.0
-	const imageWidth = 400
+	// TODO: Code needs to be optimized
+	// NOTE: Currently Rendering at a lower resolution than in the book
+	const aspectRatio = 3.0 / 2.0
+	const imageWidth = 600 // 1200
 	const imageHeight = int(imageWidth / aspectRatio)
 	const outputFile = "./output.ppm"
-	const samplesPerPixel = 100
+	const samplesPerPixel = 100 // 500
 	const maxDepth = 50
 
 	// World
-	world := hittable_list{}
-	var materialGround material = &lambertian{color{0.8, 0.8, 0.0}}
-	var materialCenter material = &lambertian{color{0.1, 0.2, 0.5}}
-	var materialLeft material = &dielectric{1.5}
-	var materialRight material = &metal{color{0.8, 0.6, 0.2}, 0.0}
-
-	world.Add(&sphere{point3{0.0, -100.5, -1.0}, 100.0, &materialGround})
-	world.Add(&sphere{point3{0.0, 0.0, -1.0}, 0.5, &materialCenter})
-	world.Add(&sphere{point3{-1.0, 0.0, -1.0}, 0.5, &materialLeft})
-	world.Add(&sphere{point3{-1.0, 0.0, -1.0}, -0.45, &materialLeft})
-	world.Add(&sphere{point3{1.0, 0.0, -1.0}, 0.5, &materialRight})
+	world := randomScene()
 
 	// Camera
-	lookfrom := point3{3, 3, 2}
-	lookat := point3{0, 0, -1}
+	lookfrom := point3{13, 2, 3}
+	lookat := point3{0, 0, 0}
 	vup := vec3{0, 1, 0}
-	dist := lookfrom.Sub(&lookat)
-	distToFocus := dist.Length()
-	aperture := 2.0
+	distToFocus := 10.0
+	aperture := 0.1
 	cam := NewCamera(lookfrom, lookat, vup, 20,
 		aspectRatio, aperture, distToFocus)
 
 	// Render
-	file, err := os.Create(outputFile)
-	if err != nil {
-		log.Fatal(err)
-	}
-	writer := bufio.NewWriter(file)
-
-	linesToWrite := []string{
-		"P3",
-		fmt.Sprintf("%d %d", imageWidth, imageHeight),
-		"255",
-	}
-
-	for j := imageHeight; j >= 0; j-- {
+	linesToWrite := make([]string, imageHeight*imageWidth)
+	index := 0
+	for j := imageHeight - 1; j >= 0; j-- {
 		for i := 0; i < imageWidth; i++ {
 			pixelColor := color{0, 0, 0}
 			for s := 0; s < samplesPerPixel; s++ {
@@ -63,11 +96,18 @@ func writePPM() {
 				rayColor := Ray_Color(&r, &world, maxDepth)
 				pixelColor = pixelColor.Add(&rayColor)
 			}
-			toWrite := WriteColor(&pixelColor, samplesPerPixel)
-			linesToWrite = append(linesToWrite, toWrite)
+			linesToWrite[index] = WriteColor(&pixelColor, samplesPerPixel)
+			index++
 		}
 	}
 
+	// Write to file
+	file, err := os.Create(outputFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	writer := bufio.NewWriter(file)
+	writer.WriteString(fmt.Sprintf("P3\n%d %d\n255\n", imageWidth, imageHeight))
 	for _, line := range linesToWrite {
 		_, err := writer.WriteString(line + "\n")
 		if err != nil {
@@ -75,8 +115,15 @@ func writePPM() {
 		}
 	}
 	writer.Flush()
+
 }
 
 func main() {
-	writePPM()
+	start := time.Now()
+
+	render()
+
+	t := time.Now()
+	elapsed := t.Sub(start)
+	fmt.Println("Elapsed Time", elapsed.Seconds(), "seconds")
 }
